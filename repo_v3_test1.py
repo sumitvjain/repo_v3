67840eb68833abc18ab49a2517dcf95de3ee71a3
Sumit Saktepar <sumitvjain@yahoo.com>
2024-06-23 02:21:55 +0530

    test1 [1.0.0]

---
+++ repo_v3_test1.py
@@ -0,0 +1,13 @@
+"""
+created new repository [repo_v3]
+then cloned it by using commnad [ git clone <https path>]
+cloned by git bash terminal.
+main branch automatically will create on remote and local.
+i hv created new .py file in local main branch.
+now need to add, commit and publish.
+first click on git tab which is on left side tool bar
+for add file click on plus symbol [staging area]
+then type comment [progress of the file]
+then need to push file.
+now file on same branch on remote.
+"""
