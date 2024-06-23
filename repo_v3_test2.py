8e3357f3262714865633a156fa79bb3ada908ff3
Sumit Saktepar <sumitvjain@yahoo.com>
2024-06-23 12:36:51 +0530

    test3 [1.0.1]

--- repo_v3_test2.py
+++ repo_v3_test2.py
@@ -1,4 +1,6 @@
 """
 now created new file version test2
 testing for commit and push with test1
+
+now testing with test3 
 """
--- repo_v3_test3.py
+++ repo_v3_test3.py
@@ -2,4 +2,10 @@
 this is repo test 3 file
 now testing for commit and push but right now test1 and test2 has not been modified.
 and testing while commit and push test1 & test2 will push or not on this commit.
+
+After create test3 at the time commit, there has not changed anything in test1 & test2 file. And after push- checked on remote main branch test3 [1.0.0] visible 
+and had not add test1 or test2 file on this commit.
+
+now checking all modified file will attach or not at the time of commit and push.
+
 """
